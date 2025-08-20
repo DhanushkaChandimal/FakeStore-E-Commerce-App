d868110704997c79ac3eb859174a80d0e4f0bc21
Dhanushka <dhanushkamcr@gmail.com>
2025-08-20 17:55:45 -0500

    Align price on cart items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -5,12 +5,12 @@ import Button from "react-bootstrap/Button";
 const CartItem = ({ product, onRemove }) => {
 
     return (
-        <Card className="d-flex flex-row align-items-center justify-content-between pe-3">
+        <Card className="d-flex flex-row align-items-center justify-content-between pe-3 cart-item">
             <Card.Body className="d-flex align-items-center gap-4">
                 <Card.Img variant="top" src={product.image} className="cart-image"/>
-                <Card.Title className="title">{product.title}</Card.Title>
-                <Card.Title>${product.price}</Card.Title>
+                <Card.Text className="title">{product.title}</Card.Text>
             </Card.Body>
+            <Card.Title className="me-2">${product.price}</Card.Title>
             <Button onClick={() => onRemove(product.id)} className="ms-auto">Remove</Button>
         </Card>
   );
